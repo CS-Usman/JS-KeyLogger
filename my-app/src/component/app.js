ecc7692bd004a04e3338ba96f55f237ad0eceb88
Muhammad Usman Mubashir <89721173+CS-Usman@users.noreply.github.com>
2022-06-14 05:24:52 -0700

    structure of landing page

--- my-app/src/component/app.js
+++ my-app/src/component/app.js
@@ -1,11 +1,67 @@
-import React from "react";
+import React,{Component} from "react";
+import "../../src/login.css"
 
-function  App() {
-    return (
-        <div>
-            <h1>Hello World!</h1>
-        </div>
-    );
+class  App extends Component{
+    constructor(){
+        super()
+        this.state = {
+            username : " ",
+            password : ""
+        }
+    }
+    handleUserName = (event)=>{
+        this.setState({
+            username:event.target.value
+        })
+    }
+    handlePassword = (event)=>{
+        this.setState({
+            password:event.target.value
+        })
+    }
+
+    render(){
+        return (
+            <div >
+                <div>
+                    <h1>JS. <br/> KeyLogger</h1>
+                </div>
+                <form>
+                    <div>
+                        <input
+                        type="email"
+                        placeholder="Username"
+                        value = {this.state.username}
+                        onChange ={this.handleUserName}
+                        />
+                    </div>
+                    <div>
+                        <input
+                        type = "password"
+                        placeholder="Password"
+                        value = {this.state.password}
+                        onChange = {this.handlePassword}
+                        />
+                    </div>
+                    <div>
+                        <p>Forgot password</p>
+                    </div>
+                    <div>
+                        <button type = "submit">Login</button>
+                        <button type = "button">Register</button>
+                    </div>
+                    <div>
+                        <p>or you can login with</p>
+                    </div>
+                    <div>
+                        <h3>f</h3>
+                        <h3>G</h3>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+   
 }
 
 export default App;
